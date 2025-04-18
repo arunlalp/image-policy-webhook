6356da49a799cf46349dc05860a637c9c7ec9c56
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2025-04-18 15:35:37 +0530

    update

---
+++ image-policy-webhook/image_policy_webhook.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/validate', methods=['POST'])
+def validate_image():
+    allowed_registries = ['registry.example.com', 'docker.io']
+
+    admission_review = request.get_json()
+    pod = admission_review['request']['object']
+
+    for container in pod['spec']['containers']:
+        image = container['image']
+        registry = image.split('/')[0]
+
+        if registry not in allowed_registries:
+            return jsonify({
+                'apiVersion': 'admission.k8s.io/v1',
+                'kind': 'AdmissionReview',
+                'response': {
+                    'uid': admission_review['request']['uid'],
+                    'allowed': False,
+                    'status': {
+                        'message': f"Image {image} is not allowed. Allowed registries: {', '.join(allowed_registries)}"
+                    }
+                }
+            })
+
+    return jsonify({
+        'apiVersion': 'admission.k8s.io/v1',
+        'kind': 'AdmissionReview',
+        'response': {
+            'uid': admission_review['request']['uid'],
+            'allowed': True
+        }
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, ssl_context=('/app/certs/tls.crt', '/app/certs/tls.key'))
